de0c203b9808d55e1c8e9331804f183cc1d72f66
Alexander <alex.kononok@gmail.com>
2021-02-20 15:14:05 +0300

    Start project

---
+++ StreetRacing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreetRacing
+//
+//  Created by Alexander Kononok on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
